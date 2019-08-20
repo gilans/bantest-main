24afe0665fc33ba868b9f46cb25ad64ce6701554
gilberto suarez <gilbertosuarez@pop-os.localdomain>
2019-08-20 17:54:01 -0400

    first commit

---
+++ public/counter_dist/precache-manifest.b4f5b12c4b590d140e53727d2ae4461f.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f68ad264ac34d7b8fd7",
+    "url": "/static/js/react-counter.js"
+  },
+  {
+    "revision": "5f68ad264ac34d7b8fd7",
+    "url": "/static/css/react-counter.css"
+  },
+  {
+    "revision": "4f0c3c32f395d43ee36f384cfc972cd7",
+    "url": "/index.html"
+  }
+];
